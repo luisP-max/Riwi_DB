ae7ffdcee2a37c19b1e443131423ef4f18918b53
luisP-max <pinamerinoluis@gmail.com>
2026-08-26 20:43:14 -0500

    acomodando

--- src/routes/ciudad.router.ts
+++ src/routes/ciudad.router.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { getCiudades, createCiudad } from '../controllers/ciudad.controller.js';
 
 const router = express.Router();
+
 router.get('/', getCiudades);
 router.post('/', createCiudad);
 
